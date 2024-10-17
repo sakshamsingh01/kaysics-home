3b629073187ad21d46a01fc8daab3dd4a98792cf
Saksham <sakshamkaundal01@gmail.com>
2024-10-18 01:07:01 +0530

    fix

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         <div className="relative flex h-20 items-center justify-between gap-3 rounded-2xl bg-gray-900/90 px-6 shadow-md before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-gray-700 before:bg-gradient-to-r from-gray-800 via-gray-900 to-gray-800 before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] after:absolute after:inset-0 after:-z-10 after:backdrop-blur-lg">
           {/* Site branding */}
           <div className="flex items-center">
-            <Logo className="h-10 w-10" /> {/* Adjusted logo size */}
+            <Logo /> {/* Adjusted logo size */}
           </div>
 
           {/* Company Name */}
